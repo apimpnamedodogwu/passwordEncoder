f485d21daba93340488b1ccbba58954e16790a8c
apimpnamedodogwu <edenelenwoke@gmail.com>
2023-02-13 11:03:57 +0100

    intial commit

---
+++ src/test/java/eden/africa/passwordmanager/PasswordManagerApplicationTest.java
@@ -0,0 +1,12 @@
+package eden.africa.passwordmanager;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+public class PasswordManagerApplicationTest {
+    @Test
+    public void test(){}
+  
+}
